cbdb1ab2cdabb0d1c5e28276a4c4d005bdb17f8d
Jerome <fabricantejerome@gmail.com>
2025-01-13 17:54:25 +0800

    Change label and arrange by created date desc

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -27,7 +27,7 @@ class HomeController < ApplicationController
   end
 
   def search
-    @tours = Tour.where("LOWER(name) LIKE ?", "%#{params[:q].downcase}%")
+    @tours = Tour.where("LOWER(name) LIKE ?", "%#{params[:q].downcase}%").order(:created_at => :desc)
     @category_name = "Search results for '#{params[:q]}'"
   end
 end
