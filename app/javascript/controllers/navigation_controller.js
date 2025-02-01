263db88aab90340046459f5809feb8475c5d344a
Jerome <fabricantejerome@gmail.com>
2025-02-01 18:29:26 +0800

    Add sidebar for different viewport

--- app/javascript/controllers/navigation_controller.js
+++ app/javascript/controllers/navigation_controller.js
@@ -2,9 +2,11 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="navigation"
 export default class extends Controller {
+  static targets = [ "sidebar" ]
+
   initialize() {
     const topNav = document.querySelector("header > nav")
-
+    
     document.addEventListener('scroll', (e) => {
       if (window.scrollY > 0) {
         topNav.classList.add('bg-white')
@@ -12,5 +14,13 @@ export default class extends Controller {
         topNav.classList.remove('bg-white')
       }
     })
-  } 
+  }
+
+  openSidebar() {
+    this.sidebarTarget.classList.remove('hidden')
+  }
+
+  closeSidebar() {
+    this.sidebarTarget.classList.add('hidden')
+  }
 }
